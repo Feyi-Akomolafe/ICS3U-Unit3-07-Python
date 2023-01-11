f75c18189409db31304f9d2d0a7a4c7fced913dd
Mofeyifoluwa David Akomolafe <mofeyifoluwa.akomolafe@mths.ca>
2023-01-11 00:03:02 +0000

    done

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created by: Feyi Akomolafe
+# Created on: Oct 2022
+# This module contains constants for guess random number
+
+MINIMUM_AGE = 25
+MAXIMUM_AGE = 40
---
+++ grandmother.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Akomolafe
+# Created on: Oct 2022
+# This program checks if user can date the grandmother's grandchildren
+
+import constants
+
+
+def main():
+    # this function checks if user can date the grandmother's grandchildren
+
+    # input
+    user_age_as_string = input("Welcome! Please, enter your age:  ")
+    print("")
+
+    # process & output
+    try:
+        user_age = int(user_age_as_string)
+        if user_age >= constants.MINIMUM_AGE and user_age <= constants.MAXIMUM_AGE:
+            print("You are an acceptable age!")
+        else:
+            print("You are NOT an acceptable age")
+    except ValueError:
+        print("{0} is not an integer".format(user_age_as_string))
+    finally:
+        print("Done.")
+
+
+if __name__ == "__main__":
+    main()
